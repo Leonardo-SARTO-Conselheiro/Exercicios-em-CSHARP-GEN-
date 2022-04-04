898bf8663eab0b602c606493ea725e92b7f74235
unknown <leonardocasinha12@gmail.com>
2022-04-04 16:00:09 -0300

    @Leonardo-SARTO-Conselheiro - Meu primeiro Hello World

---
+++ Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_GEN_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu primeiro\nHello World!");
+        }
+    }
+}
