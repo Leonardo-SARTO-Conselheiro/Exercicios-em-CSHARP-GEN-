e527e505aa5c86d4dab4b1b560d38e548cd39f0b
unknown <leonardocasinha12@gmail.com>
2022-04-05 12:57:46 -0300

    @Leonardo-Sarto-Conselheiro - Questionamento e Cálculo de Comissão (if e else)

---
+++ Cálculo de Comissão/Cálculo de Comissão.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace VisualStudioCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Entre com o valor mensal das suas vendas:");
+            //double.parse converte a string para numeros INTeiros
+            double valorVendido = double.Parse(Console.ReadLine()); 
+
+            //se ela vender menos de 10000 a comissão dela vai ser de 0.01 
+            if(valorVendido <= 10000){
+                Console.WriteLine("O valor a receber é: R$ " + (valorVendido * 0.01));
+            }
+
+            else if (valorVendido <= 20000){
+                Console.WriteLine("O valor a receber é: R$ " + (valorVendido * 0.02));
+            }
+
+            else if (valorVendido <= 30000){
+                Console.WriteLine("O valor a receber é: R$ " + (valorVendido * 0.03));
+            }
+
+            else{
+                Console.WriteLine("O valor a receber é: R$ " + (valorVendido * 0.1));
+            }
+
+        }
+    }
+}
---
+++ Questionamento (if e else)/Questionamento (if e else).cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Aula05VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int valor = 9;
+
+            //if (valor == 5) 
+            //{
+            //Console.WriteLine("O Valor é 5");
+            //}
+
+            //else
+            //{
+            //Console.WriteLine("Não é o valor 5");
+            //}
+
+            //if e else simples
+
+            //===========================================
+
+            //bool condicaoCursoConcluido = true;
+            //int condicaoQuantidadeCursoConcluido = 26;
+            //bool condicaoQuitacao = true;
+            //bool condicaoDevolverLivro = true;
+
+            //if (condicaoCursoConcluido == true && 
+            // condicaoQuantidadeCursoConcluido >= 25 &&
+            //condicaoQuitacao == true &&
+            //condicaoDevolverLivro == true)
+            //{
+            //Console.WriteLine("Você foi aprovado");
+            //}
+            //else
+            //{
+            //Console.WriteLine("Você foi Reprovado");
+            //}
+
+            //nesse caso todas as condições foram true, então ele foi aprovado, 
+            //se eu tivesse colocado alguma com true, ele seria repovado
+
+            //==================================================================================
+
+            Console.WriteLine("Você concluiu o curso? (sim ou não): ");
+            string condicaoCursoConcluido = Console.ReadLine();
+
+            Console.WriteLine("Quantos cursos você concluiu? : ");
+            int condicaoQuantidadeCursoConcluido = Convert.ToInt32(Console.ReadLine() );
+
+            Console.WriteLine("Você pagou todas as mensalidas? (sim ou não)");
+            string condicaoQuitacao = Console.ReadLine();
+
+            Console.WriteLine("Você devolveou todos os livros? (sim ou não)");
+            string condicaoDevolverLivro = Console.ReadLine();
+
+
+
+            if (condicaoCursoConcluido == "sim"  &&
+                condicaoQuantidadeCursoConcluido >= 25 &&
+                condicaoQuitacao == "sim" &&
+                condicaoDevolverLivro == "sim")
+            {
+                Console.WriteLine("Você foi aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Você foi Reprovado");
+            }
+            
+            //Respondendo o usuario, conforme as informações inseridas por ele
+
+            }
+        }
+    }
+
