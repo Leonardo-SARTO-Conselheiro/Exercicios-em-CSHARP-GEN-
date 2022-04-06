f74e74ffb9d1ec8a6d1f7e0857e0063235a32749
unknown <leonardocasinha12@gmail.com>
2022-04-06 12:51:15 -0300

    @Leonardo-SARTO-Conselheiro - Tabuada

---
+++ Tabuada/Tabuada.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula_6_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //WriteLine printa e pula uma linha
+            Console.WriteLine("Tabuada! \n");
+
+            //uso o x++ pq estou multiplicando de 1 em 1 numero
+            for(int x = 1; x <= 10; x++)
+            {
+                for(int y = 1; y <= 10; y++)
+                {
+                    //Console.WriteLine(x + " x " + y + " = " + x * y);
+                    Console.WriteLine($"{x} x {y} = {x*y}");
+                    //codigo de cima (linha 18) esta interpolando, porem o da linha 17 tbm funciona
+
+                    // utilizando o break abaixo, eu paro a calculadora no x5
+                    if (y ==5){
+                        break;
+                    }
+                }
+
+            }
+
+        }
+    }
+}
